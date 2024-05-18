59fdead130fa7ba87ab3ca8075fa7821dcebbc11
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-18 19:28:54 +0530

    added unsigned integer

--- datatypes.c
+++ datatypes.c
@@ -4,7 +4,10 @@ int main() {
     int a;
     long b;
     short c;
+    unsigned int d = -20;
     printf("size of a %lu \n", sizeof(a));
     printf("size of b %lu \n", sizeof(b));
     printf("size of c %lu \n", sizeof(c));
+    printf("size of d %lu \n", sizeof(d));
+    return 1;
 }
