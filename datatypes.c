ef568f4bced298493c996834269149cc9d65a9ae
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-18 19:18:43 +0530

    added data types file

---
+++ datatypes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    long b;
+    short c;
+    printf("size of a %lu \n", sizeof(a));
+    printf("size of b %lu \n", sizeof(b));
+    printf("size of c %lu \n", sizeof(c));
+}
