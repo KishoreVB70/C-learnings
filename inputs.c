c3bda4c4c30bddc736fba84c1573d984084816e7
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-18 19:38:21 +0530

    added multiple inputs

--- inputs.c
+++ inputs.c
@@ -4,5 +4,10 @@ int main() {
     int number;
     printf("enter a number: ");
     scanf("%d", &number);
-    printf("the number is %d", number);
+    printf("the number is %d \n", number);
+    float boten;
+    printf("multiple inputs, first enter a int then a float \n");
+    scanf("%d%f", &number, &boten);
+    printf("number: %d, float: %f", number, boten);
+
 }
