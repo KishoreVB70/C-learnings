b3322a92e2ae581c07f4eb6b18c52e2d0edf8b48
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-19 17:57:23 +0530

    implemented break

--- loop.c
+++ loop.c
@@ -4,17 +4,23 @@ int main() {
     printf("enter the number of times to print a star: \n");
     int a;
     scanf("%d", &a);
-    // for (int i; i <= a; i++) {
-    //     printf("*\n");
-    // }
-    int i = 0;
+
+    // Modified function where the highest number of stars to print is 5
+    // User can input anything, but 10 is the max
+    for (int i; i < a; i++) {
+        if (i == 5) {
+            break;
+        }
+        printf("*\n");
+    }
+    // int i = 0;
     // while (i < a) {
     //     printf("*\n");
     //     i++;
     // }
-    do {
-        printf("*\n");
-        i++;
-    }
-    while(i < a);
+    // do {
+    //     printf("*\n");
+    //     i++;
+    // }
+    // while(i < a);
 }
