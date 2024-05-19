e18128cfeddda3e76401e76a28c546e133981ee0
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-19 17:48:25 +0530

    added while loop

--- loop.c
+++ loop.c
@@ -4,7 +4,12 @@ int main() {
     printf("enter the number of times to print a star: \n");
     int a;
     scanf("%d", &a);
-    for (int i; i <= a; i++) {
+    // for (int i; i <= a; i++) {
+    //     printf("*\n");
+    // }
+    int i = 0;
+    while (i < a) {
         printf("*\n");
+        i++;
     }
 }
