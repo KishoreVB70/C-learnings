80c34ebc4dd70ab25921dd7215b4833a4be62faf
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-19 18:09:30 +0530

    implemented continue

--- loop.c
+++ loop.c
@@ -5,14 +5,30 @@ int main() {
     int a;
     scanf("%d", &a);
 
-    // Modified function where the highest number of stars to print is 5
+    // Modified function where the highest number of stars to print is 10
     // User can input anything, but 10 is the max
-    for (int i; i < a; i++) {
-        if (i == 5) {
+    // for (int i; i < a; i++) {
+    //     if (i == 10) {
+    //         break;
+    //     }
+    //     printf("*\n");
+    // }
+
+    // Another function to demonstrate continue
+
+    for (int i = 1; i <= a; i++) {
+        // Will print till 10
+        if(i == 11) {
             break;
         }
-        printf("*\n");
+        // Will not print the num 4
+        if(i == 4) {
+            continue;
+        }
+        printf("%d\n", i);
     }
+
+
     // int i = 0;
     // while (i < a) {
     //     printf("*\n");
