28a8c0155bf5e1a55b91f0e224c9602c8e1b0522
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-23 21:51:35 +0530

    moved cube function outside of the main function

--- funciton.c
+++ funciton.c
@@ -4,8 +4,9 @@ int main() {
     int input;
     printf("enter number to be cubed: ");
     scanf("%d", &input);
-    int cuber(int num) {
-        return num * num * num;
-    }
     printf(cuber(input));
+}
+
+int cuber(int num) {
+    return num * num * num;
 }
