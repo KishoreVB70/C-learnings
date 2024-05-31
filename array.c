8893c78339e1d7f7cdf770754a0c18bb807699b5
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-31 13:23:09 +0530

    code to loop through the array manually and print it

--- array.c
+++ array.c
@@ -3,6 +3,8 @@
 int main() {
     int myArr[20] = {1,2,3,4,5};
     myArr[0] = 0;
-    printf("%d", myArr[0]);
+    for (int i; i < 5; i++) {
+        printf("%d", myArr[i]);
+    }
     return 0;
 }
