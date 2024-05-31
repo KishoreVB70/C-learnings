2d528e50fbc7a4cad0f913faf9f0d6335749d079
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-31 13:45:26 +0530

    added a 2 dimensional array

--- array.c
+++ array.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int myArr[20] = {1,2,3,4,5};
-    myArr[0] = 0;
-    for (int i; i < 5; i++) {
-        printf("%d", myArr[i]);
+    // One dimensional array
+    // int myArr[20] = {1,2,3,4,5};
+    // myArr[0] = 0;
+    // for (int i; i < 5; i++) {
+    //     printf("%d", myArr[i]);
+    // }
+
+    //Multi dimensional array
+    int multi[2][3] = {{1,2,3}, {4,5,222}};
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d\n", multi[i][j]);
+        }
     }
+
     return 0;
 }
