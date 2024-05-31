7406344bca86e5c8917207ca0c2e856444773903
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-31 10:37:22 +0530

    added 4 different types of functions

--- function.c
+++ function.c
@@ -1,24 +1,88 @@
 #include <stdio.h>
 int cuber(int num);
-void isEven(int num);
+void isEvenNoArgumentNoReturn();
+void isEvenNoReturn(int num);
+char isEvenNoArgument();
+char isEven(int num);
 
 int main() {
+    // First function
+    printf("Running the first function void of any arguments and return type\n");
+    isEvenNoArgumentNoReturn();
+    printf("\nEnd of the first function\n");
+
+    // Second function
+    printf("Running the second function void of any arguments but with a return type\n");
+    char noArgument = isEvenNoArgument();
+    printf("result of the No argument function is %c \n", noArgument);
+
+    // Get the input 
+    printf("Now you need to give just one input number for the next 2 functions\n");
     int input;
-    printf("enter number to be checked: ");
+    printf("Enter number to be checked: ");
     scanf("%d", &input);
-    isEven(input);
+
+    // Third function
+    printf("Start of the function with no return but an argument\n");
+    isEvenNoReturn(input);
+    printf("\nfunction with no return ends\n");
+
+    // Fourth function
+    printf("Start of the full function with both the return and the argument\n");
+    char full = isEven(input);
+    printf("result of the full function is %c \n", full);
+
+    // End
+    printf("End of all functions");
+    return 0;
 }
 
-int cuber(int num) {
-    return num * num * num;
-}
-
-void isEven(int num) {
+// Function with no argument and no return type
+void isEvenNoArgumentNoReturn() {
+    int num;
+    printf("Enter number to be checked: ");
+    scanf("%d", &num);
     if (num % 2 == 0) {
         printf("even");
     } else if (num == 0 ) {
         printf("nither even nor odd");
     } else {
         printf("odd");
+    }
+}
+
+// Function that doesn't return anything
+void isEvenNoReturn(int num) {
+    if (num % 2 == 0) {
+        printf("even");
+    } else if (num == 0 ) {
+        printf("nither even nor odd");
+    } else {
+        printf("odd");
+    }
+}
+
+// Function with return but doesn't have argument 
+char isEvenNoArgument() {
+    int num;
+    printf("enter number to be checked: ");
+    scanf("%d", &num);
+    if (num % 2 == 0) {
+        return 'e';
+    } else if (num == 0 ) {
+        return '0';
+    } else {
+        return 'o';
+    }
+}
+
+// Function with both argument and return
+char isEven(int num) {
+    if (num % 2 == 0) {
+        return 'e';
+    } else if (num == 0 ) {
+        return '0';
+    } else {
+        return 'o';
     }
 }
--- sort.c
+++ sort.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int nums[5]  = {1,2,3,4};
+    int nums[4]  = {4,3,2,1};
+    // Implementing split sort
+    for (int x; x < nums; x++) {
+        
+    }
 }
