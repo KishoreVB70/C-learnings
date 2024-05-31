a23952a78450a52e0b4df8ee943e5a1c2a7e483e
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-31 13:48:27 +0530

    added 3 dimensional array and loop to iterate through it and print

--- array.c
+++ array.c
@@ -9,13 +9,21 @@ int main() {
     // }
 
     //Multi dimensional array
-    int multi[2][3] = {{1,2,3}, {4,5,222}};
+    int multi[2][3][2] = {
+        {
+            {1,9}, {3,5}, {5,6}
+        }, 
+        {
+            {7,8}, {2,10}, {11,12}
 
+        }
+    };
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 3; j++) {
-            printf("%d\n", multi[i][j]);
+            for (int k = 0; k < 2; k++) {
+                printf("%d", multi[i][j][k]);
+            }
         }
     }
-
     return 0;
 }
