78f212f4eb77920a64905a0b740ac8a60e0b6e79
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-31 13:40:38 +0530

    modified the int into a double to tackle the rounding off

--- average.c
+++ average.c
@@ -4,6 +4,7 @@ int main() {
     int n = 0;
     int sum = 0;
     int myArray[20];
+    double avg;
     printf("enter a total num of values to calculate\n");
     scanf("%d", &n);
 
@@ -13,7 +14,10 @@ int main() {
         sum+=myArray[i];
     }
 
-    printf("average is: %d\n", sum/n);
+    // Data type casting of sum variable from int to double 
+    avg = (double) sum/n;
+
+    printf("average is: %.2lf\n", avg);
     printf("Printing all the elements\n");
 
     for (int i; i < n; i++) {
