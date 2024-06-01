fb1f3da2c2baecd3a128bf392ed742e0770e8538
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-01 19:01:57 +0530

    added function to understand arrays and pointers

--- pointers.c
+++ pointers.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+int printAddressOfArray(int arr[], int size);
+
 
 int main() {
-    int bot = 500;
-    int* add = &bot;
-    printf("Address of the variable: %p\n", add);
-    printf("Value of the variable according to the pointer: %d\n", *add);
-    *add = 10;
-    printf("Value of the variable according to it: %d", bot);
+    int arr[] = {1,2,3,4,5};
+    printf("Address of the array in main function: %p\n", &arr);
+    printf("The value which is stored in the addres is %p\n", arr);
+    printAddressOfArray(arr, 5);
+
     return 0;
+}
+
+int printAddressOfArray(int arr[], int size) {
+    printf("The address of the array itself is %p\n", &arr);
+    printf("The value stored in the array thing is %p\n", arr);
+    printf("The value which is stored in the addres is %d\n", *arr);
+
+    for (int i = 0; i < size; i++) {
+        printf("Value of element %d is %d, and the address is %p\n", i, arr[i], &arr[i]);
+    }
 }
