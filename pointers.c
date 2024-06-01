a66860e944c6be3556302d372f1e362bc693a2cf
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-01 13:50:04 +0530

    access the value of the pointer address

--- pointers.c
+++ pointers.c
@@ -3,6 +3,7 @@
 int main() {
     int bot = 500;
     int* add = &bot;
-    printf("Address of the variable is: %p", add);
+    printf("Address of the variable is: %p\n", add);
+    printf("Value of the variable is: %d", *add);
     return 0;
 }
