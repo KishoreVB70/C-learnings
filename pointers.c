3df68e7878b97032815a1abde131585e264b842d
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-01 13:55:05 +0530

    check pointer pointing to the value

--- pointers.c
+++ pointers.c
@@ -3,7 +3,11 @@
 int main() {
     int bot = 500;
     int* add = &bot;
-    printf("Address of the variable is: %p\n", add);
-    printf("Value of the variable is: %d", *add);
+    printf("Address of the variable before value change: %p\n", add);
+    printf("Value of the variable according to the pointer: %d\n", *add);
+    bot = 20;
+    printf("Address of the variable after value change: %p\n", add);
+    printf("Value of the variable according to the pointer: %d\n", *add);
+    printf("Value of the variable: %d", bot);
     return 0;
 }
