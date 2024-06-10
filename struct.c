7a27a2b168aea9251bb0bdaa4efb3add6a4840f9
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-10 14:02:37 +0530

    added 2 ways of initializing a structy

--- struct.c
+++ struct.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-struct nullPointer {
+
+struct Mine {
     int number;
     float noomber;
     double noooomber;
     char hi;
+// One way of creating struct is that declaring the name after the struct itself
 }boteson;
 
 int main() {
@@ -12,10 +14,20 @@ int main() {
     boteson.noomber = 5.2;
     boteson.noooomber = 5.23145;
 
-    printf("my char: %c\n", boteson.hi);
-    printf("my number: %d\n", boteson.number);
-    printf("my noomber: %f\n", boteson.noomber);
-    printf("my noooomber: %f", boteson.noooomber);
+    // Another way of creating struct
+    struct Mine bot;
+    bot.hi = 'a';
+    bot.number = 1;
+    bot.noomber = 5.2;
+    bot.noooomber = 5.23145;
+
+
+
+
+    printf("my char: %c\n", bot.hi);
+    printf("my number: %d\n", bot.number);
+    printf("my noomber: %f\n", bot.noomber);
+    printf("my noooomber: %f", bot.noooomber);
     
     return 0;
 }
