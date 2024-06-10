0ce04b7d41c7abfdc1be98afdc0d6e52150ea274
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-10 15:21:41 +0530

    added another struct as a nested struct

--- struct.c
+++ struct.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+struct Posting {
+    char location[50];
+    char role[50];
+
+};
+
 struct User {
     int userID;
     char lastName[50];
     int salary;
+    struct Posting posting;
 // One way of creating struct is that declaring the name after the struct itself
 }priya;
 
@@ -12,18 +19,23 @@ typedef struct User2 {
     int userID;
     char lastName[50];
     int salary;
+    struct Posting posting;
 }user;
 
 int main() {
     user ranjina;
     ranjina.userID = 9;
     ranjina.salary = 0;
+    strcpy(ranjina.posting.location, "Erode");
+    strcpy(ranjina.posting.role, "Student");
     strcpy(ranjina.lastName, "Ranjith");
 
     priya.userID = 1;
     priya.salary = 30000;
     // strcopy must only be used to store string
     strcpy(priya.lastName, "Arjunan");
+    strcpy(priya.posting.role, "Business developer");
+    strcpy(priya.posting.location, "Banglore");
 
     // Another way of creating struct
     struct User divya;
@@ -32,8 +44,8 @@ int main() {
     strcpy(divya.lastName, "Divya");
 
     printf("Salary of Ranjina: %d \n", ranjina.salary);
-    printf("Last name of Priya: ");
-    puts(priya.lastName);
+    printf("Role of Priya: ");
+    puts(priya.posting.role);
     
     return 0;
 }
