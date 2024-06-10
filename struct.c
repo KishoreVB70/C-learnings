a400db616f31a664405e1ca362dccf207f86893c
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-10 14:20:09 +0530

    added another struct using typedef keyword

--- struct.c
+++ struct.c
@@ -8,7 +8,18 @@ struct User {
 // One way of creating struct is that declaring the name after the struct itself
 }priya;
 
+typedef struct User2 {
+    int userID;
+    char lastName[50];
+    int salary;
+}user;
+
 int main() {
+    user ranjina;
+    ranjina.userID = 9;
+    ranjina.salary = 0;
+    strcpy(ranjina.lastName, "Ranjith");
+
     priya.userID = 1;
     priya.salary = 30000;
     // strcopy must only be used to store string
@@ -20,7 +31,7 @@ int main() {
     divya.salary = 20000;
     strcpy(divya.lastName, "Divya");
 
-    printf("Salary of Divya: %d \n", divya.salary);
+    printf("Salary of Ranjina: %d \n", ranjina.salary);
     printf("Last name of Priya: ");
     puts(priya.lastName);
     
