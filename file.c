12a8695dc43155c4ee3034cd6e7de660861cfb46
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-12 20:59:15 +0530

    added code to read the first word of a string file

--- file.c
+++ file.c
@@ -9,13 +9,21 @@ int main() {
         exit(1);
     }
 
-    fprintf(file, "Number is %d", 42);
+    fprintf(file, "Number is %d ", 42);
     fclose(file);
 
     FILE *fle = fopen("./file.txt", "a");
 
     fprintf(fle, "And another num is 52");
     fclose(fle);
+
+    fle = fopen("./file.txt", "r");
+
+    char big[70];
+    fscanf(fle, "%s", big);
+
+    puts(big);
+    
 
     return 0;
 }
