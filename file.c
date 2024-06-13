a51d589aa414b84d74176be07bbebbc67aa874f3
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-13 06:25:48 +0530

    added code to write to a binary file

--- file.c
+++ file.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+void binFile();  
+void textFile();  
 
 int main() {
+    binFile();
+
+    return 0;
+}
+
+void binFile() {
+    FILE *file = fopen("./file.bin", "wb");
+
+    if(file == NULL) {
+        printf("Error! ");
+        exit(1);
+    }
+
+    int nu = 42;
+    fwrite(&nu, sizeof(int), 1, file);
+    
+    fclose(file);
+    int new;
+
+
+    file = fopen("./file.bin", "rb");
+
+
+
+    fread(&new, sizeof(int), 1, file);
+
+    printf("New: %d", new);
+
+    fclose(file);
+}
+
+void textFile() {
     FILE *file = fopen("./file.txt", "w");
 
     if(file == NULL) {
@@ -9,21 +43,18 @@ int main() {
         exit(1);
     }
 
-    fprintf(file, "Number is %d ", 42);
+    fprintf(file, "It is %d ", 42);
     fclose(file);
 
-    FILE *fle = fopen("./file.txt", "a");
+    file = fopen("./file.txt", "a+");
 
-    fprintf(fle, "And another num is 52");
-    fclose(fle);
+    fprintf(file, "And another num is 52");
+    fclose(file);
 
-    fle = fopen("./file.txt", "r");
+    file = fopen("./file.txt", "r");
 
     char big[70];
-    fscanf(fle, "%s", big);
+    fscanf(file, "%s", big);
 
     puts(big);
-    
-
-    return 0;
 }
