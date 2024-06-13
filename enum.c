adf641912eb9f16549d858d21521a27e7690bb64
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-13 08:58:19 +0530

    modifed the enum to contain HTTP server codes using a switch statement

--- enum.c
+++ enum.c
@@ -4,13 +4,38 @@
 
 
 enum status {
+  processing = 102,
   resolved = 200,
-  rejected = 400  
-}buildStatus;
+  movedPermanently = 301,
+  unauthorized = 401,
+  notFound = 404,
+  serverError = 500  
+}serverStatus;
 
 int main() {
-    buildStatus = resolved;
-    printf("Status = %d\n", buildStatus);
-    buildStatus = rejected;
-    printf("Status = %d", buildStatus);
+    serverStatus = notFound;
+
+    switch (serverStatus)
+    {
+        case 102:
+            printf("server is processing");
+            break;
+        case 200:
+            printf("OKKK");
+            break;
+        case 301:
+            printf("Redirecting as it is moved permanently");
+            break;
+        case 401:
+            printf("You are unauthorized");
+            break;
+        case 404:
+            printf("What you're looking for is not found");
+            break;
+        case 500:
+            printf("Sorry, server error");
+            break;
+        default:
+            break;
+    }
 }
